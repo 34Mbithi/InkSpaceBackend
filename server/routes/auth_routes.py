6cff881f926bb3a87225c95714f2e02f382c5aae
vin <vinnymummo@gmail.com>
2024-10-27 02:38:33 +0300

    commit

--- server/routes/auth_routes.py
+++ server/routes/auth_routes.py
@@ -17,7 +17,7 @@ auth_ns = Namespace('auth', description='Authentication related operations')
 def generate_jwt_token(user):
     payload = {
         'user_id': user.id,
-        'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=1) 
+        'exp': datetime.utcnow() + timedelta(hours=1)  
     }
     token = jwt.encode(payload, current_app.config['SECRET_KEY'], algorithm='HS256')
     return token
